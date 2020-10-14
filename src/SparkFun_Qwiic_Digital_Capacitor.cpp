f83be52e9a4c130cb8988a6f7a9558f1c214f339
pmakin-png <54557724+pmakin-png@users.noreply.github.com>
2020-10-14 16:01:30 -0600

    Update SparkFun_Qwiic_Digital_Capacitor.cpp

--- src/SparkFun_Qwiic_Digital_Capacitor.cpp
+++ src/SparkFun_Qwiic_Digital_Capacitor.cpp
@@ -17,3 +17,37 @@ bool digitalCap::isConnected()
         return true;
     return false;
 }
+
+bool digitalCap::writeVolatileCapacitance(uint8_t capacitance)
+{
+    //TODO: figure out how to separate bytes
+    _i2cPort->beginTransmission(_deviceAddress);
+    _i2cPort->write(byte2);
+    _i2cPort->write(byte1);
+    if (_i2cPort->endTransmission() == 0)
+        return true;
+    return false;
+}
+
+uint8_t digitalCap::readVolatileCapacitance()
+{
+    uint8_t temp = 0;
+    uint8_t count = 0;
+    _i2cPort->requestFrom(_deviceAddress, 4);
+    while(_i2cPort->available())
+    {
+        temp = temp | (_i2cPort->read() << count*8);    //Try to concatenate all four bytes into one number, datasheet pg 17
+        count++;
+    }
+
+}
+
+bool digitalCap::setNonVolatileMode()
+{
+    _i2cPort->beginTransmission(_deviceAddress);
+    _i2cPort->write(0x90);  //From datasheet, pg 18
+    _i2cPort->write(0xFF);  //These are don't care bits
+    if (_i2cPort->endTransmission() == 0)
+        return true;
+    return false;
+}
